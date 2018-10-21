e208ac0c1e3ce87242f1618a469d32b381ee8fca
Horacio Guzmán Parra <hgp691@hotmail.com>
2018-10-20 22:47:06 -0500

    Initial Commit

---
+++ pruebaHero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pruebaHero
+//
+//  Created by Horacio Guzman on 10/20/18.
+//  Copyright © 2018 hero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ pruebaHeroTests/pruebaHeroTests.swift
@@ -0,0 +1,34 @@
+//
+//  pruebaHeroTests.swift
+//  pruebaHeroTests
+//
+//  Created by Horacio Guzman on 10/20/18.
+//  Copyright © 2018 hero. All rights reserved.
+//
+
+import XCTest
+@testable import pruebaHero
+
+class pruebaHeroTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
